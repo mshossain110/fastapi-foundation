43f75176aaeabfca828156039c79e3d1abe9167f
mshossain110 <mshossain110@gmail.com>
2026-05-02 22:55:26 +0600

    Refactor error handling models for consistency and clarity

--- fastapi_foundation/models/api_response.py
+++ fastapi_foundation/models/api_response.py
@@ -1,38 +1,40 @@
 from typing import Generic, TypeVar, List, Optional, Any, Dict
-from pydantic import BaseModel, Field
-from pydantic.generics import GenericModel
-
+from pydantic import BaseModel
 T = TypeVar('T')
+
 
 class ErrorItem(BaseModel):
     message: Optional[str] = None
-    
+
     class Config:
         populate_by_name = True
-        alias_generator = lambda s: s.lower()
+        def alias_generator(s): return s.lower()
+
 
 class ErrorDescription(BaseModel):
     errors: Optional[List[Optional[ErrorItem]]] = None
-    
+
     class Config:
         populate_by_name = True
-        alias_generator = lambda s: s.lower()
+        def alias_generator(s): return s.lower()
+
 
 class Error(BaseModel):
     message: Optional[str] = None
     title: Optional[str] = None
     description: Optional[ErrorDescription] = None
-    
+
     class Config:
         populate_by_name = True
-        alias_generator = lambda s: s.lower()
+        def alias_generator(s): return s.lower()
 
-class ApiResponse(GenericModel, Generic[T]):
+
+class ApiResponse(BaseModel, Generic[T]):
     status: Optional[str] = None
     code: Optional[int] = None
     error: Optional[Error] = None
     data: Optional[T] = None
-    
+
     class Config:
         populate_by_name = True
-        alias_generator = lambda s: s.lower()
+        def alias_generator(s): return s.lower()
